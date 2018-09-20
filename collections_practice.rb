8517f4e4928e4a8d69abf5565df5e3adb6d36817
Will Figueiredo <will@wfigueiredo.com>
2018-09-20 02:22:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+end
+
+def sort_array_char_count(array)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+end
+
+def kesha_maker(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end
